5aeb223c7d8d561fba6343ee2a1460287cd79841
Tylerdevries <tylerdevries365@gmail.com>
2022-09-26 01:30:03 +0000

    Add urls.py to base app, connected app to project via settings.py

--- todo_list/base/models.py
+++ todo_list/base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(user, on_delete=models.CASCADE, null=True, blank=True)
+    title =
+    description =
+    complete =
+    create =
